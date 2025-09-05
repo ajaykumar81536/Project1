d77838f7285977a715e062c204b8b1ea8a1fa80b
ajaykumar81536 <146960066+ajaykumar81536@users.noreply.github.com>
2025-09-05 18:13:37 +0530

    Delete PYTHON_TU directory

--- PYTHON_TU/SETs_Practise/SET_3/Q6.py
+++
@@ -1,8 +0,0 @@
-# Example list
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# Using list comprehension to find the sum of squares of odd numbers
-sum_of_squares_odd = sum(x ** 2 for x in my_list if x % 2 != 0)
-
-print("List:", my_list)
-print("Sum of squares of odd numbers:", sum_of_squares_odd)
